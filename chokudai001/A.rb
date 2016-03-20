2ba393a04c5d28f90c83a0bafb4a8aa9517c236c
nicklegr <addr.for+github@gmail.com>
2016-03-20 13:11:20 +0900

    AC 541032

---
+++ chokudai001/A.rb
@@ -0,0 +1,85 @@
+require 'pp'
+
+def ppd(*arg)
+  if $DEBUG
+    pp(*arg)
+  end
+end
+
+def putsd(*arg)
+  if $DEBUG
+    puts(*arg)
+  end
+end
+
+def parr(arr)
+  puts arr.join(" ")
+end
+
+def parrd(arr)
+  putsd arr.join(" ")
+end
+
+def ri
+  readline.to_i
+end
+
+def ris
+  readline.split.map do |e| e.to_i end
+end
+
+def rs
+  readline.chomp
+end
+
+def rss
+  readline.chomp.split
+end
+
+def rf
+  readline.to_f
+end
+
+def rfs
+  readline.split.map do |e| e.to_f end
+end
+
+def rws(count)
+  words = []
+  count.times do
+    words << readline.chomp
+  end
+  words
+end
+
+# main
+t_start = Time.now
+
+cases = 1
+
+(1 .. cases).each do |case_index|
+  arr = []
+  30.times do
+    arr << ris
+  end
+
+  for y in 0...30
+    for x in 0...30
+      arr[y][x].times do
+        puts "#{y+1} #{x+1}"
+      end
+    end
+  end
+
+  # progress
+  trigger = 
+    if cases >= 10
+      case_index % (cases / 10) == 0
+    else
+      true
+    end
+
+  if trigger
+    STDERR.puts("case #{case_index} / #{cases}, time: #{Time.now - t_start} s")
+  end
+end
